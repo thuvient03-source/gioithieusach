e6f63b0320f98310e1c377a7d0031ac304978c66
thuvient03-source <buiquangtan20@gmail.com>
2026-09-07 21:17:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/gioithieusach/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
